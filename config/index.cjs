4cba0497624c95e8113ca8c742ca623aa0d25dbf
HuaJiLiang <160691222+HuaJiLiang@users.noreply.github.com>
2024-02-28 15:10:27 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,6 +6,8 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
+  // 请将以下代码按照图示粘贴到对应位置
+ USE_PASSAGE: 'push-plus',
 
   // 使用微信测试号：公众号APP_ID
   APP_ID: 'wx912b501741717fd9',
@@ -21,7 +23,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '亲爱的姐姐',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o8UhY6otOCXGmHOD7HYZHmZD7m2c',
+      id: '0743921f82b94e41ba4ff43e6ed9e917',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'FopLOiO2tJlYMRXywjgZM2roLFoLMV5nidP57QOlFug',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
