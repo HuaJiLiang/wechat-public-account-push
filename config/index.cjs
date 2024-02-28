9cd9f777c8fb99d6deca3f28738c4b8faea2af68
HuaJiLiang <160691222+HuaJiLiang@users.noreply.github.com>
2024-02-28 15:34:38 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -7,7 +7,7 @@
  */
 const USER_CONFIG = {
   // 请将以下代码按照图示粘贴到对应位置
- USE_PASSAGE: 'push-plus',
+ USE_PASSAGE: 'server-chan',
 
   // 使用微信测试号：公众号APP_ID
   APP_ID: '',
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '亲爱的宝贝儿',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '0743921f82b94e41ba4ff43e6ed9e917',
+      id: 'SCT240694TumaqlXS5TV7bybnSiJd8UmHA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
